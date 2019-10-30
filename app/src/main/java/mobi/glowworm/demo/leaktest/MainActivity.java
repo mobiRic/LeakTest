d90f0f5445fc7177707913e07e511d7a99b88365
Richard Le Mesurier <richard@glowworm.mobi>
2019-10-30 11:41:14 +0200

    Add Navigation Controller

--- app/src/main/java/mobi/glowworm/demo/leaktest/MainActivity.java
+++ app/src/main/java/mobi/glowworm/demo/leaktest/MainActivity.java
@@ -1,15 +1,21 @@
 package mobi.glowworm.demo.leaktest;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
 
 import android.os.Bundle;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private NavController navController;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        navController = Navigation.findNavController(this, R.id.nav_host_fragment);
     }
 }

13f3e40f0d9cf2af2e7940fc082a5aeb020c0b12
Richard Le Mesurier <richard@glowworm.mobi>
2019-10-30 11:14:43 +0200

    Add blank fragment

--- app/src/main/java/mobi/glowworm/demo/leaktest/MainActivity.java
+++ app/src/main/java/mobi/glowworm/demo/leaktest/MainActivity.java
@@ -7,13 +7,9 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tvHello;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        tvHello = findViewById(R.id.tvHello);
     }
 }
